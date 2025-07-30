5e663e05d357a9080279aabe6f396b10124b7ce4
JACOB CHARLES FINNEY SUBBIAH <jacobcharlesfinney_subbiah@lincolnelectric.in>
2025-07-30 11:39:59 +0000

    ok to commit

--- project1/webapp/controller/Main.controller.js
+++ project1/webapp/controller/Main.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], (Controller) => {
     "use strict";
-// Comment
+
     return Controller.extend("project1.controller.Main", {
         onInit() {
         }
